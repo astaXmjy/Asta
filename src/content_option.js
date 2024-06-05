af48c370df048c02d628697829d4f0840f8762f9
astaXmjy <128385847+astaXmjy@users.noreply.github.com>
2024-06-05 22:52:54 +0530

    Add files via upload

---
+++ src/content_option.js
@@ -0,0 +1,144 @@
+const logotext = "Aadarsh";
+const meta = {
+  title: "Aadarsh Yadav",
+  description:
+    "I’m Aadarsh Yadav Full stack devloper,currently looking for work",
+};
+
+const introdata = {
+  title: "I’m Aadarsh yadav",
+  animated: {
+    first: "I love to code",
+    second: "I love to make cool websites",
+    third: "I love to watch Anime ",
+  },
+  description: "I'm a full stack developer from Bharat 😄.",
+};
+
+const dataabout = {
+  title: "abit about my self",
+  aboutme:
+    "A developer who is on journey to develop something out of this world by continously improving skills and learning new things. I love to explore mountains and forest alone.",
+};
+
+const skills = [
+  {
+    name: "Python",
+    value: 70,
+  },
+  {
+    name: "Django",
+    value: 70,
+  },
+  {
+    name: "Javascript",
+    value: 60,
+  },
+  {
+    name: "React",
+    value: 60,
+  },
+  {
+    name: "C++",
+    value: 80,
+  },
+  {
+    name: "Tailwind css",
+    value: 65,
+  },
+  {
+    name: "Fast Api",
+    value: 60,
+  },
+  {
+    name: "Html and Css",
+    value: 70,
+  },
+  {
+    name: "Docker",
+    value: 60,
+  },
+  {
+    name: "Git and GitHub",
+    value: 70,
+  },
+  {
+    name: "Django Rest Framework",
+    value: 65,
+  },
+];
+
+const services = [
+  {
+    title: "Web Apps",
+    description:
+      "Making cool websites production ready with best ui and ux experience,safe and secure to use.",
+  },
+  {
+    title: "Python related works",
+    description:
+      "We do all python related works like making an apps using python frameworks.",
+  },
+];
+
+const dataportfolio = [
+  {
+    description:
+      "Andromeda ai made by using gemini and assembly api which takes commands from voice and give results in voice format.",
+    link: "https://github.com/astaXmjy/Andromeda_ai.git",
+  },
+  {
+    description:
+      "django blog app using bootstrap",
+    link: "https://github.com/astaXmjy/django_app.git",
+  },
+  {
+    description:
+      "Insta clone using django,html,css and javascript",
+    link: "https://github.com/astaXmjy/instaclone-django.git",
+  },
+  {
+    description:
+      "CRUD api using FASTAPI",
+    link: "https://github.com/astaXmjy/api-using-python.git",
+  },
+  {
+    description:
+      "Menu card  using React",
+    link: "https://github.com/astaXmjy/react-proj-1.git",
+  },
+  {
+    description:
+      "text extraction from documents by using django and pytesseract engine",
+    link: "https://github.com/astaXmjy/docAuth1.git",
+  },
+];
+
+const contactConfig = {
+  YOUR_EMAIL: "yadavadarshgamer@gmail.com",
+  YOUR_FONE: "+91 7828941907",
+  description:
+    "I'm happy to work for your projects and i'll give my 100% to deliver your work perfectly and within time limit.Stay happy Hare Krishna.",
+  // creat an emailjs.com account
+  // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+  YOUR_SERVICE_ID: "service_g0m9l71",
+  YOUR_TEMPLATE_ID: "template_7d2oawo",
+  YOUR_USER_ID: "GvpCobv_pXpRJ8PgW",
+};
+
+const socialprofils = {
+  github: "https://github.com/astaXmjy",
+  linkedin: "https://linkedin.com",
+  twitter: "https://twitter.com",
+};
+export {
+  meta,
+  dataabout,
+  dataportfolio,
+  skills,
+  services,
+  introdata,
+  contactConfig,
+  socialprofils,
+  logotext,
+};
